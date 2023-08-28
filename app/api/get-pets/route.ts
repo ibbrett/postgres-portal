d864f6d4f9189020f9c835b2de6ad4ecb94fad09
Brett Morriss <brettmorriss@gmail.com>
2023-08-28 13:05:44 -1000

    Delete commented code

--- app/api/get-pets/route.ts
+++ app/api/get-pets/route.ts
@@ -6,8 +6,6 @@ export async function GET(request: Request) {
   try {
     const pets = await sql`SELECT * FROM Pets;`;
     return NextResponse.json({ pets: pets.rows }, { status: 200 });
-    // this doesn't work
-    // return JSON.stringify(NextResponse.json({ pets: pets.rows }, { status: 200 }), null, 2);
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
