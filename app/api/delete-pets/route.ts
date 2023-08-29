af00e346241ec041fb0af137fa765d56056c0c33
Brett Morriss <brettmorriss@gmail.com>
2023-08-28 15:12:21 -1000

    Update api file

--- app/api/delete-pets/route.ts
+++ app/api/delete-pets/route.ts
@@ -2,6 +2,7 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 // change this to Id
+// add note here
  
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
